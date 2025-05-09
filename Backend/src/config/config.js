b6cfbda4cf14e813c164278dd930eb75501e8334
Mahia Umbarila <mahia07112006@gmail.com>
2025-05-08 23:18:02 -0500

    Proyecto para desplegar

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -4,16 +4,10 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config({path: '../../.env'});
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: 'mysql',
-    logging: false
-  }
-);
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'mysql',
+  logging: false
+});
 
 export default sequelize;
