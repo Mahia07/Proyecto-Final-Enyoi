5df7b96efaa3c79be45a2adf0c26037c51b864ae
Mahia Umbarila <mahia07112006@gmail.com>
2025-05-16 07:46:42 -0500

    cambios de proyecto

--- Frontend/react/src/Api/api.js
+++ Frontend/react/src/Api/api.js
@@ -229,7 +229,15 @@ export const resetPassword = async (token, password) => {
       body: JSON.stringify({ password }),
     });
 
+    const contentType = response.headers.get("Content-Type");
+
+    if (!contentType || !contentType.includes("application/json")) {
+      const text = await response.text(); 
+      throw new Error(`Respuesta no JSON: ${text}`);
+    }
+
     const data = await response.json();
+
     if (!response.ok) throw new Error(data.message || "Error al cambiar contraseña");
 
     return data;
@@ -238,6 +246,7 @@ export const resetPassword = async (token, password) => {
     throw error;
   }
 };
+
 
 export const createCategory = async ({ name, description }) => {
   try {
