17d5839222f72b82b8e8107f6e5cc1cdb32a2493
Mahia Umbarila <mahia07112006@gmail.com>
2025-05-16 17:40:06 -0500

    nuevos cambios para solucion de bugs

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(cors({
     origin: [
-        'https://proyecto-final-enyoi-qvop.vercel.app/'
+        'https://proyecto-final-enyoi-qvop.vercel.app'
     ] 
     
   }));
